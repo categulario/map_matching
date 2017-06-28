04bc80aceb107f40e73c5e5429d2de769bfdaa2a
Abraham Toriz Cruz <awonderfulcode@gmail.com>
2017-06-28 17:30:47 -0500

    functions for working with vectors in lua

--- lua/ways_from_gps.lua
+++ lua/ways_from_gps.lua
@@ -4,6 +4,47 @@ local rad = ARGV[1]
 local lon = ARGV[2]
 local lat = ARGV[3]
 
+-- vector subtraction
+local function vsub(v1, v2)
+	return {v1[1]-v2[1], v1[2]-v2[2]}
+end
+
+-- vector inner product
+local function dot(v1, v2)
+	local ret = 0
+
+	for i = 1, #v1 do
+		ret = ret + a[i] * b[i]
+	end
+
+	return ret
+end
+
+-- vector scalar product
+local function prod(a, v)
+	res = {}
+
+	for i = 1, #v do
+		res[#res+1] = v[i]*a
+	end
+
+	return res
+end
+
+-- gets parameter `a` that describes the projection of `p` in line n1->n2
+-- in terms of linear combination of n1 and n2
+local function get_projection(n1, n2, p)
+	local tn2 = vsub(n2, n1)
+	local tp = vsub(p, n1)
+
+	local sol = prod(
+		dot(tp, tn2)/dot(tn2, tn2),
+		tn2
+	)
+
+	return sol[1] / tn2[1]
+end
+
 redis.call('del', 'tmp:gps:ways')
 
 -- add to set all ways near a gps position
