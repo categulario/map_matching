c033d6c7623bd73eb560ba8723ac828a3c8881cf
Abraham Toriz <awonderfulcode@gmail.com>
2018-10-26 11:40:31 -0500

    lets start refreshing this repo

---
+++ Makefile
@@ -0,0 +1,9 @@
+.PHONY: pytest lint test
+
+test: pytest lint
+
+pytest:
+	pytest -xvv
+
+lint:
+	flake8 --exclude=.env,.tox,dist,docs,build,*.egg .
--- overpass/get_street_graph.sh
+++ overpass/get_street_graph.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl --data-urlencode data@payload.overpassql  http://overpass-api.de/api/interpreter > street_graph.json
+curl --data-urlencode data@payload.overpassql http://overpass-api.de/api/interpreter > street_graph.json
