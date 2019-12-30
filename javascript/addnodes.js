99087ee86b7b0b1641baf44fdf13594339a41db4
Abraham Toriz <awonderfulcode@gmail.com>
2019-12-30 08:20:24 -0600

    retab

--- javascript/addnodes.js
+++ javascript/addnodes.js
@@ -1,16 +1,16 @@
 // for use inside the geojson console
 (function () {
-	var gps_icon = L.icon({iconUrl: 'http://categulario.tk/marker.png', iconAnchor: [10, 10]})
-	var street_icon = L.icon({iconUrl: 'http://categulario.tk/marker.png', iconAnchor: [10, 10]})
-	var api = window.api;
+  var gps_icon = L.icon({iconUrl: 'http://categulario.tk/marker.png', iconAnchor: [10, 10]})
+  var street_icon = L.icon({iconUrl: 'http://categulario.tk/marker.png', iconAnchor: [10, 10]})
+  var api = window.api;
 
-	api.data.get('map').features.forEach(function (feature) {
-		if (feature.geometry.type == 'LineString') {
-			feature.geometry.coordinates.forEach(function (coord) {
-				L.marker(coord.reverse(), {
-					icon: gps_icon,
-				}).addTo(api.map);
-			});
-		}
-	});
+  api.data.get('map').features.forEach(function (feature) {
+    if (feature.geometry.type == 'LineString') {
+      feature.geometry.coordinates.forEach(function (coord) {
+        L.marker(coord.reverse(), {
+          icon: gps_icon,
+        }).addTo(api.map);
+      });
+    }
+  });
 })();
